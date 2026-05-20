b0db0b9d614553cd29567dc64921b99c559ad5ad
kheourtsokhy <naknak6412@gmail.com>
2026-05-20 17:34:30 +0700

    fix home_screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_first_project/config/functions/function.dart';
-import 'package:flutter_first_project/config/theme/theme_style.dart';
 import 'package:flutter_first_project/screen/subject_detail.dart';
-import 'package:flutter_first_project/widget/bottom_navigation_bar_widget.dart';
+import 'package:flutter_first_project/widget/card_home_widget.dart';
 import 'package:flutter_first_project/widget/date_time_line_widget.dart';
 import 'package:flutter_first_project/widget/schedule_card.dart';
 
-class HomeSmall extends StatefulWidget {
-  const HomeSmall({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<HomeSmall> createState() => _HomeSmallState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeSmallState extends State<HomeSmall> {
+class _HomeScreenState extends State<HomeScreen> {
   int currentIndex = 0;
 
   void bottomNavigationBarTap(int index) {
     setState(() {
       currentIndex = index;
+    });
+  }
+
+  DateTime selectDate = DateTime.now();
+
+  void onChageDate(DateTime dateValue) {
+    setState(() {
+      selectDate = dateValue;
     });
   }
